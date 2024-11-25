0324cc07e1b0b054883d5fcb0e1a7468c52aef68
Anakion <andreygolosov786@gmail.com>
2024-11-25 13:29:43 +0200

    Add env.py connect Base.metadata and first version migrations

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,6 +7,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from app.core.config import settings
+from app.db.base import Base
+from app.models.user import User
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,7 +24,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
+config.set_main_option('sqlalchemy.url', settings.URL_DB)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
