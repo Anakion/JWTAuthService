575418d3d5b56e360f43b8bd77848350beb28c06
Anakion <andreygolosov786@gmail.com>
2024-11-25 13:07:50 +0200

    Changing a synchronous session to an asynchronous session

--- app/services/db_service.py
+++ app/services/db_service.py
@@ -1,26 +1,32 @@
 from typing import Union
-from sqlalchemy.orm import Session
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.security import get_password_hash, verify_password
 from app.models.user import User
 from app.schemas.user import UserCreate, UserInDB
 
 
-def create_user(db: Session, user_create: UserCreate) -> User:
+async def create_user(db: AsyncSession, user_create: UserCreate) -> User:
     user = User(username=user_create.username,
                 email=user_create.email,
                 hashed_password=get_password_hash(user_create.password),
                 role=user_create.role)
     db.add(user)
-    db.commit()
-    db.refresh(user)
+    await db.commit()
+    await db.refresh(user)
     return user
 
-def get_user(db: Session, username: str) -> Union[User, None]:
-    return db.query(User).filter(User.username == username).first()
 
-def authenticate_user(db: Session, username: str, password: str) -> Union[UserInDB, None]:
-    user = get_user(db, username)
+async def get_user(db: AsyncSession, username: str) -> Union[User, None]:
+    result = await db.execute(select(User).filter(User.username == username))
+    user = result.scalar_one_or_none()
+    return user
+
+
+async def authenticate_user(db: AsyncSession, username: str, password: str) -> Union[UserInDB, None]:
+    user = await get_user(db, username)
     if user and verify_password(password, user.hashed_password):
         return UserInDB(**user.__dict__)
     return None
